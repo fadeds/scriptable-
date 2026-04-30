d4abe8ccedbdd8c9a2ebb0379c7fb5957ba3bda8
Fadeds <1154250793@qq.com>
2026-04-30 13:04:53 +0000

    fix: 在下载函数中添加下载日志以便于调试

--- Script/install/installer.js
+++ Script/install/installer.js
@@ -27,6 +27,7 @@ Script.Installer = async function () {
   // ========== 下载函数 ==========
   async function downloadScript(fileName) {
     var url = REPO_BASE +'/'+ fileName
+    console.log("正在下载 " + REPO_BASE +'/'+ fileName + " ...")
     var req = new Request(url)
     req.timeoutInterval = 30
     var code = await req.loadString()
